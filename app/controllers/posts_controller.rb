5b9068bfb4d62c0731fdab7cd937f8ed1c02c974
natsu <tamanatsu89@icloud.com>
2019-06-27 15:21:54 +0700

    layoutchange

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,16 @@
 class PostsController < ApplicationController
     before_action :authenticate_user!, only: :new
+    before_action :set_post, only: [:show, :destroy]
 
     def index
-        @posts = Post.all.order(created_at: 'desc')
+        @posts = Post.all
     end
 
     def show
-        @post = Post.find(params[:id])
+        @posts = Post.all
+
+        @next_post = @post.next
+        @prev_post = @post.prev
     end
 
     def new
@@ -28,12 +32,16 @@ class PostsController < ApplicationController
     end
 
     def destroy
-        @post = Post.find(params[:id])
         @post.destroy
         redirect_to posts_path
     end
 
     private
+    def set_post
+        @post = Post.find(params[:id])
+    end
+
+
     def post_params
         params.require(:post).permit(:content, :answer)
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
    belongs_to :user
     validates :content, presence: true
     validates :answer, presence: true
+
+    def next
+        Post.where("id > ?", id).first
+    end
+
+    def prev
+        Post.where("id < ?", id).last
+    end
 end
